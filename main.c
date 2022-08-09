2b5a925250b0d0f68766b7bc9882482f89e71521
Luis Peres <luisfvperes@gmail.com>
2022-08-09 10:00:24 -0300

    making program compatible with any file size

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./include/sorter.h"
-#define BUFFER_LENGTH 104
+#define BUFFER_LENGTH MAX_SEQUENCE_LENGTH * (SIGNIFICANT_DIGITS+1)
 
 int main(int argc, char const *argv[])
 {
+    if (argc < 3)
     {
+        printf("Missing arguments. Terminal input should be in the form:\n");
+        printf("\"./sequenceSorter <input-file-name>.csv <output-file-name>.csv\"\n");
+        return 0;
+    }
+    char const *inputFilename = argv[1];
+    char const *outputFilename = argv[2];
 
     FILE *inputFile = fopen(inputFilename, "r");
     FILE *outputFile = fopen(outputFilename, "w");
+    
+    fseek(inputFile, 0, SEEK_END);
+    int fileSize = ftell(inputFile);
+    fseek(inputFile, 0, SEEK_SET);
 
-    while (fgets(buffer, BUFFER_LENGTH, inputFile))
+    char *buffer = malloc(fileSize);
+
+    while (fgets(buffer, fileSize, inputFile))
     {
         char *newLine = sorter(buffer);
         fputs(newLine, outputFile);
@@ -18,5 +31,6 @@ int main(int argc, char const *argv[])
     fclose(inputFile);
     fclose(outputFile);
     free(buffer);
+    // buffer = realloc(buffer, 0);
     return 0;
 }
